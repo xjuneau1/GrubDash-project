459845514c4db6a6a835b012a667489814cba3d1
Xander Juneau <xanderjuneau@gmail.com>
2022-09-06 15:22:01 -0400

    completed dish routes all tests passing

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -9,6 +9,62 @@ function list (req, res, send) {
     res.json({data: dishes})
 }
 
+function read(req, res, next){
+    const { dishId } = req.params
+    const foundDish = dishes.find((dish)=> dish.id === dishId)
+    res.json({data: foundDish})
+}
+
+function create(req, res, next) {
+    const {data:{name, description, price, image_url} = {} } = req.body
+    const newDish = {
+        id: nextId(),
+        name: name,
+        description: description,
+        price: price,
+        image_url: image_url
+    }
+    dishes.push(newDish)
+    return res.status(201).json({data: newDish})
+}
+
+function update(req, res, next){
+    const { dishId } = req.params
+    const { data: { name, description, image_url} = {} } = req.body
+    const foundDish = dishes.find((dish)=> dish.id === dishId)
+
+    foundDish.name = name
+    foundDish.description = description
+    foundDish.image_url = image_url
+    
+   res.json({data: foundDish})
+}
+
+function validateDishExists (req, res, next){
+    const { dishId } = req.params
+    const foundDish = dishes.find((dish)=> dish.id === dishId)
+    if(foundDish){
+        return next()
+    }
+    next({
+        status: 404,
+        message: `Dish does not exist: ${dishId}`
+    })
+}
+
+function validateBodyIdEqualsRouteId (req, res, next){
+    const { dishId } = req.params
+    const {data: {id} } = req.body
+
+    if (id === dishId || !id){
+        return next()
+    }
+    next({
+        status: 400,
+        message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`
+    })
+}
+
 function bodyDataHas(propertyName){
     return function (req, res, next) {
         const {data = {} } = req.body
@@ -33,18 +89,6 @@ function priceIsValidInteger (req, res, next) {
     next()
 }
 
-function create(req, res, next) {
-    const {data:{name, description, price, image_url} = {} } = req.body
-    const newDish = {
-        id: nextId(),
-        name: name,
-        description: description,
-        price: price,
-        image_url: image_url
-    }
-    dishes.push(newDish)
-    return res.status(201).json({data: newDish})
-}
 
 module.exports = {
     list,
@@ -56,4 +100,18 @@ module.exports = {
         priceIsValidInteger,
         create,
     ],
+    update: [
+        validateDishExists,
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        priceIsValidInteger,
+        validateBodyIdEqualsRouteId,
+        update
+    ],
+    read: [
+        validateDishExists,
+        read
+    ]
 }
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -9,4 +9,10 @@ router
     .post(controller.create)
     .all(methodNotAllowed)
     
+router
+    .route("/:dishId")
+    .get(controller.read)
+    .put(controller.update)
+    .all(methodNotAllowed)
+    
 module.exports = router;
