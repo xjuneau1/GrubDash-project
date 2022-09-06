526b21faf5cf1b274f12cf2611a82a022330d1db
Xander Juneau <xanderjuneau@gmail.com>
2022-09-06 13:05:28 -0400

    finished post routes and middleware for /dishes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,17 +1,59 @@
 const path = require("path");
-
-// Use the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"));
 
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
 
 function list (req, res, send) {
     res.json({data: dishes})
 }
 
+function bodyDataHas(propertyName){
+    return function (req, res, next) {
+        const {data = {} } = req.body
+        if(data[propertyName]) {
+            return next()
+        }
+        next({
+            status: 400,
+            message: `Dish must include a ${propertyName}`
+        })
+    }
+}
+
+function priceIsValidInteger (req, res, next) {
+    const {data:{price}} = req.body
+    if(price < 0 || !Number.isInteger(price)){
+        next({
+            status: 400,
+            message: `Dish must have a price that is an integer greater than 0`
+        })
+    }
+    next()
+}
+
+function create(req, res, next) {
+    const {data:{name, description, price, image_url} = {} } = req.body
+    const newDish = {
+        id: nextId(),
+        name: name,
+        description: description,
+        price: price,
+        image_url: image_url
+    }
+    dishes.push(newDish)
+    return res.status(201).json({data: newDish})
+}
+
 module.exports = {
     list,
+    create: [
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        priceIsValidInteger,
+        create,
+    ],
 }
